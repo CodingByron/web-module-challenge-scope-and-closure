693e20ed4e2997161ef58dd5bce43e441b01801c
Byron Sanders <byrone_sanders@hotmail.com>
2023-06-22 13:16:24 -0700

    test git push

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
-//❗❗ PLEASE READ THE README file for project instructions, helpful resources, additional tasks and stretch problems, and more ❗❗ 
+//❗❗ PLEASE READ THE README file for project instructions, helpful resources, additional tasks and stretch problems, and more ❗❗
 
 // ⭐️ Example Challenge START ⭐️
 
 /**Example Task : processFirstItem()
  * This example shows how you might go about solving the rest of the tasks
- * 
+ *
  * Use the higher order function processFirstItem below to do the following:
  *  1. Receive an array of strings in a parameter
  *  2. Receive a callback function that takes a string as its argument in a parameter
- *  3. Return the result of invoking the callback function and passing in the FIRST 
+ *  3. Return the result of invoking the callback function and passing in the FIRST
  *     element in the array as the argument
- * 
+ *
  * The following code is demonstrating a way of completing this task
  * It returns the string `foofoo`
-*/
+ */
 
 function processFirstItem(stringList, callback) {
-  return callback(stringList[0])
+  return callback(stringList[0]);
 }
-console.log('example task:', processFirstItem(['foo','bar'],function(str){return str+str}));
+console.log(
+  "example task:",
+  processFirstItem(["foo", "bar"], function (str) {
+    return str + str;
+  })
+);
 
 // ⭐️ Example Challenge END ⭐️
-
 
 ///// M V P ///////
 
@@ -51,8 +55,8 @@ console.log('example task:', processFirstItem(['foo','bar'],function(str){return
 function counterMaker() {
   let count = 0;
   return function counter() {
-   return count++;
-  }
+    return count++;
+  };
 }
 
 const counter1 = counterMaker();
@@ -64,7 +68,6 @@ function counter2() {
   return count++;
 }
 
-
 /* ⚾️⚾️⚾️ Task 2: inning() ⚾️⚾️⚾️
 Use the inning function below to do the following:
   1. Return a random whole number of points between 0 and 2 scored by one team in an inning
@@ -74,10 +77,9 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(){
+function inning() {
   return Math.floor(Math.random() * Math.floor(3));
 }
-
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
 Use the finalScore function below to do the following:
@@ -91,22 +93,21 @@ Use the finalScore function below to do the following:
   "Home": 11,
   "Away": 5
 }
-*/ 
+*/
 
-function finalScore(inningCallBack, innings){
+function finalScore(inningCallBack, innings) {
   let homeScore = 0;
   let awayScore = 0;
-  for(let i = 0; i < innings; i++){
+  for (let i = 0; i < innings; i++) {
     homeScore = homeScore + inningCallBack();
     awayScore = awayScore + inningCallBack();
   }
   return {
-    Home: homeScore, Away: awayScore
-  }
+    Home: homeScore,
+    Away: awayScore,
+  };
 }
 console.log(finalScore(inning, 9));
-
-
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
@@ -120,11 +121,11 @@ For example: invoking getInningScore(inning) might return this object:
 }
   */
 
-
 function getInningScore(inningCallBack) {
-  return{
-    Home: inningCallBack(), Away: inningCallBack()
-  }
+  return {
+    Home: inningCallBack(),
+    Away: inningCallBack(),
+  };
 }
 console.log(inning);
 
@@ -172,28 +173,31 @@ function scoreboard(inningScoreCallBack, inningCallBack, inningNumbers) {
   const scoreByInnings = [];
   let homeScore = 0;
   let awayScore = 0;
-  for (let i = 0; i < inningNumbers; i++){
+  for (let i = 0; i < inningNumbers; i++) {
     const currentInning = inningScoreCallBack(inningCallBack);
-    homeScore = homeScore + currentInning.Home
-    awayScore = awayScore + currentInning.Away
-    scoreByInnings.push(`Inning ${i + 1}: Away ${currentInning.Away} - Home ${currentInning.Home}`)
+    homeScore = homeScore + currentInning.Home;
+    awayScore = awayScore + currentInning.Away;
+    scoreByInnings.push(
+      `Inning ${i + 1}: Away ${currentInning.Away} - Home ${currentInning.Home}`
+    );
   }
-  if(homeScore === awayScore){
-    scoreByInnings.push(`This game requires extra innings: Away ${currentInning.Away} - Home ${currentInning.Home}`);
-  }else {
+  if (homeScore === awayScore) {
+    scoreByInnings.push(
+      `This game requires extra innings: Away ${currentInning.Away} - Home ${currentInning.Home}`
+    );
+  } else {
     scoreByInnings.push(`Final Score: Away: ${awayScore} - Home: ${homeScore}`);
   }
-  return scoreByInnings; 
+  return scoreByInnings;
 }
 console.log(scoreboard(getInningScore, inning, 9));
 
-
-
+//comment test
 
 /* 🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑 */
-function foo(){
-  console.log('its working');
-  return 'bar';
+function foo() {
+  console.log("its working");
+  return "bar";
 }
 foo();
 module.exports = {
@@ -205,4 +209,4 @@ module.exports = {
   finalScore,
   getInningScore,
   scoreboard,
-}
+};
